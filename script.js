2b79a5847487071c085c87de771c9ef0d6e3dd95
Jake Kile <jkile9@gmail.com>
2020-02-10 00:20:20 -0700

    made placeholder pages to test page changes

--- script.js
+++ script.js
@@ -3,13 +3,22 @@ let timer = 10;
 function decrementTimer(){
     setInterval(function(){
         timer--;
-        document.getElementById("timer").innerText = `Time: ${timer}`;
-        if(timer == 0){
-            alert("Time is up!");
-        }
+        // document.getElementById("timer").innerText = `Time: ${timer}`;
+        // if(timer == 0){
+        //    alert("Time is up!");
+        // }
     }, 1000);
 
 }
 
-decrementTimer();
+document.getElementById("startQuiz").addEventListener("click", function(){
+    document.getElementById("home").style = "display: none";
+    document.getElementById("formOne").style = "display: block";
+    decrementTimer();
+})
 
+document.getElementById("formOne").addEventListener("click", function(e){
+    if(e.target.tagName == "INPUT"){
+        document.getElementById("formOne").style = "display: none";
+    }
+})
